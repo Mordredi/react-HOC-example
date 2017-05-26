440b266e82a30c22ecf200d0940db88ee947c28b
Arthur <arthur.p.wright@gmail.com>
2017-05-25 22:11:16 -0400

    Set up initial routing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,24 @@
 /* eslint react/prefer-stateless-function: "off" */
+
 import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+import { Login } from './login';
+import { Home } from './home';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello World!!!</h1>
+      <Router>
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </main>
+      </Router>
     );
   }
 }
